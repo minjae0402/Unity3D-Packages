328c2f696f64a842f6a8041699db1a8fc207eeda
minjae0402 <kangjack0402@gmail.com>
2024-12-08 13:31:17 +0900

    가장 가까운 오브젝트 판별

--- Assets/Scripts/Easy/GameControl.cs
+++ Assets/Scripts/Easy/GameControl.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class GameControl : MonoBehaviour
@@ -6,6 +7,7 @@ public class GameControl : MonoBehaviour
     [SerializeField] LayerMask layer;
     [SerializeField] float radius;
     [SerializeField] Collider[] col;
+    [SerializeField] Transform target;
 
     void Start()
     {
@@ -15,5 +17,24 @@ public class GameControl : MonoBehaviour
     void enemyaround()
     {
         col = Physics.OverlapSphere(player.transform.position, radius, layer);
+
+        Transform short_enemy = null;
+
+        if (col.Length > 0)
+        {
+            float short_distance = Mathf.Infinity;
+
+            foreach (Collider s_col in col)
+            {
+                float playertoenemy = Vector3.SqrMagnitude(player.transform.position - s_col.transform.position);
+
+                if (short_distance > playertoenemy)
+                {
+                    short_distance = playertoenemy;
+                    short_enemy = s_col.transform;
+                }
+            }
+        }
+        target = short_enemy;
     }
 }
